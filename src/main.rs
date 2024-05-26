eea52d9fe1e3d52d763c68b191e43359fc29e088
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-05-26 19:10:29 +0200

    template added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,71 @@
+struct TokenAmount(u64);
+struct StakedTokenAmount(u64);
+struct LpTokenAmount(u64);
+struct Price(u64);
+struct Percentage(u64);
+
+struct LpPool {
+    price: Price,
+    token_amount: TokenAmount,
+    st_token_amount: StakedTokenAmount,
+    lp_token_amount: LpTokenAmount,
+    liquidity_target: TokenAmount,
+    min_fee: Percentage,
+    max_fee: Percentage,
+}
+
+enum Errors {
+    InvalidFee,
+    InsufficientLiquidity,
+    InsufficientLpTokens,
+    Other(String),
+}
+
+impl LpPool {
+    pub fn init(
+        price: Price,
+        min_fee: Percentage,
+        max_fee: Percentage,
+        liquidity_target: TokenAmount,
+    ) -> Result<Self, Errors> {
+        // TODO:
+        // State change for all LpPool vars
+        // Returns instance of LpPool
+
+        if min_fee.0 > max_fee.0 {
+            return Err(Errors::InvalidFee);
+        } else {
+            panic!("Aaaa");
+        }
+        // PROVIDE IMPLEMENTATION
+    }
+
+    pub fn add_liquidity(
+        self: &mut Self,
+        token_amount: TokenAmount,
+    ) -> Result<LpTokenAmount, Errors> {
+        // TODO:
+        // State change for all LpPool vars
+        // Returns instance of LpPool
+
+        // PROVIDE IMPLEMENTATION
+    }
+
+    pub fn remove_liquidity(
+        self: &mut Self,
+        lp_token_amount: LpTokenAmount,
+    ) -> Result<(TokenAmount, StakedTokenAmount), Errors> {
+        // PROVIDE IMPLEMENTATION
+    }
+
+    pub fn swap(
+        self: &mut Self,
+        staked_token_amount: StakedTokenAmount,
+    ) -> Result<TokenAmount, Errors> {
+        // PROVIDE IMPLEMENTATION
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
