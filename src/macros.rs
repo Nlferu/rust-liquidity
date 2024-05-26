b07b95fae891da4dc8e84c1a3ff607b4ba2c8951
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-05-26 21:53:33 +0200

    macros removed

--- src/macros.rs
+++
@@ -1,9 +0,0 @@
-macro_rules! impl_add_assign {
-    ($($t:ty)*) => ($(
-        impl std::ops::AddAssign for $t {
-            fn add_assign(&mut self, other: Self) {
-                self.0 += other.0;
-            }
-        }
-    )*)
-}
