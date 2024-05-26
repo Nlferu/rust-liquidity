c357f2e9cec76b86ff6efccd26a34ae6c3c65233
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-05-26 17:56:05 +0200

    initial setup

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+// Marinade -> liquid staking protocol on the Solana. It allows to divide your stake among many validators instead of just 1 of top 32.
+// mSOL -> liquidity token that represents staked SOL (It is pegged to the value of SOL)
+// EPOCH -> 2 days
